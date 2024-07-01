1afe8f4ac5ad377eda60724af66e37a2a62b0a5d
André Robles <155944246+andreroblesb@users.noreply.github.com>
2024-06-30 22:14:51 -0600

    Descriptive message about the changes

---
+++ static/js/main.js
@@ -0,0 +1,40 @@
+// Function for Modal that displays the contact methods
+// Bulma framework already provides a sample Javascript, so part of the job was already done. 
+document.addEventListener('DOMContentLoaded', () => {
+  // To open and close the modal 
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+  
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+  
+    // Calls to open
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+      const modal = $trigger.dataset.target;
+      const $target = document.getElementById(modal);
+  
+      $trigger.addEventListener('click', () => {
+        openModal($target);
+      });
+    });
+  
+    // Calls to close 
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+  
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+
+    // To remove from the DOM 
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      const $notification = $delete.parentNode;
+  
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });
